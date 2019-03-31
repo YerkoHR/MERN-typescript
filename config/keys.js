dc49639627c5a9b8b018b7e692e7e518f525208c
Waimog <yerko.anton.roj@hotmail.com>
2019-03-31 16:58:11 -0400

    mongodb cluster updated

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoAtlasURI:
-    "mongodb://yerkoahr:Maltesefalc0n@eo-classes-shard-00-00-eardg.mongodb.net:27017,eo-classes-shard-00-01-eardg.mongodb.net:27017,eo-classes-shard-00-02-eardg.mongodb.net:27017/test?ssl=true&replicaSet=eo-classes-shard-0&authSource=admin&retryWrites=true"
+    "mongodb://yerkoahr:Maltesefalc0n@yerkocluster-shard-00-00-eardg.mongodb.net:27017,yerkocluster-shard-00-01-eardg.mongodb.net:27017,yerkocluster-shard-00-02-eardg.mongodb.net:27017/test?ssl=true&replicaSet=YerkoCluster-shard-0&authSource=admin&retryWrites=true"
 };
