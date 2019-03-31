1183b29edf9e622ce39c6e393e1c1f521d03fa62
Waimog <yerko.anton.roj@hotmail.com>
2019-03-31 12:51:03 -0400

    typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const path = requite("path");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
