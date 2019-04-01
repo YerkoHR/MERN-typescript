f924bfa97ab2e621700b45114e10aeb8bfee5b06
Waimog <yerko.anton.roj@hotmail.com>
2019-04-01 15:59:03 -0400

    auth logic done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
 // User model
 const User = require("../../models/user");
@@ -47,6 +48,16 @@ router.post("/", (req, res) => {
       );
     });
   });
+});
+
+// @route GET api/auth/user
+// @desc Get user data
+// @access Private
+
+router.get("/user", auth, (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then(user => res.json(user));
 });
 
 module.exports = router;
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 const Item = require("../../models/item");
 
@@ -12,8 +13,9 @@ router.get("/", (req, res) => {
 
 // @route POST api/items
 // @desc Create an Item
+// @access Private
 
-router.post("/", (req, res) => {
+router.post("/", auth, (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
@@ -23,8 +25,9 @@ router.post("/", (req, res) => {
 
 // @route POST api/items/:id
 // @desc Delete an Item
+// @access Private
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", auth, (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
